f8166d06639e605a1cdbb0ad739b053a71997cb4
zjiang714 <jiangzhiwei0809@gmail.com>
2026-03-09 21:48:03 +0800

    main.py相关库增加

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import torch
 
 
 from py38learn_demo.read_data_set import read_data_set
+from py38learn_demo.read_data_set import create_data
 
 
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     read_data_set()
+
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
